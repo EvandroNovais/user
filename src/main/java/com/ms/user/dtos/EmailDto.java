56ac38f110f3f28fc78a5b491c6bd9c124e9d508
Evandro Oliveira de Novais <evandro-novais@outlook.com>
2023-10-19 15:12:25 -0300

    Created Producer and EmailDto

---
+++ src/main/java/com/ms/user/dtos/EmailDto.java
@@ -0,0 +1,11 @@
+package com.ms.user.dtos;
+
+import java.util.UUID;
+
+public class EmailDto {
+
+    private UUID userId;
+    private String emailTo;
+    private String subject;
+    private String message;
+}
